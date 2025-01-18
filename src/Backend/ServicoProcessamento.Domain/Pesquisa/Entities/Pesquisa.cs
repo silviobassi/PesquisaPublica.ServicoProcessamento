49fc04d5f6c1f876a65678aba2576b9bd0d76646
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2025-01-18 00:08:39 -0300

    feature: criando campo ativa para pesquisa

--- src/Backend/ServicoProcessamento.Domain/Pesquisa/Entities/Pesquisa.cs
+++ src/Backend/ServicoProcessamento.Domain/Pesquisa/Entities/Pesquisa.cs
@@ -4,12 +4,18 @@ public sealed class Pesquisa
 {
     public string Id { get; set; } = string.Empty;
     public string Codigo { get; init; } = string.Empty;
-    
+
     // alterar para DateTimeOffset
     public DateTime Inicio { get; init; }
     public DateTime Fim { get; init; }
+    public bool Ativa { get; private set; } = true;
+    public bool Expirada => DateTime.Now > Fim;
     public List<Pergunta>? Perguntas { get; init; } = [];
 
+    public void Desativar() => Ativa = false;
+    
+    public void Ativar() => Ativa = true;
+    
     public void AddPergunta(Pergunta pergunta)
     {
         Perguntas?.Add(pergunta);
--- src/Backend/ServicoProcessamento.Infrastructure/Data/Migrations/MongoDbConfigurator.cs
+++ src/Backend/ServicoProcessamento.Infrastructure/Data/Migrations/MongoDbConfigurator.cs
@@ -10,31 +10,6 @@ public static class MongoDbConfigurator
 {
     public static void Configure()
     {
-        /*BsonClassMap.RegisterClassMap<Respondedor>(cm =>
-        {
-            cm.AutoMap();
-            cm.MapIdMember(r => r.Id)
-                .SetIdGenerator(CombGuidGenerator.Instance)
-                .SetSerializer(new GuidSerializer(GuidRepresentation.Standard));
-            cm.MapMember(r => r.Nome).SetIsRequired(true);
-            cm.MapMember(r => r.Email).SetIsRequired(true);
-            cm.MapMember(r => r.Idade).SetIsRequired(true);
-        });
-
-        BsonClassMap.RegisterClassMap<Alternativa>(cm =>
-        {
-            cm.AutoMap();
-            cm.MapIdMember(c => c.Id)
-                .SetIdGenerator(CombGuidGenerator.Instance)
-                .SetSerializer(new GuidSerializer(GuidRepresentation.Standard));// Converte ObjectId para string
-            cm.MapMember(a => a.Descricao).SetIsRequired(true);
-            cm.MapMember(a => a.RespondedorIds)
-                .SetSerializer(
-                    new EnumerableInterfaceImplementerSerializer<List<Guid>>(
-                        new GuidSerializer(GuidRepresentation.Standard)));
-        });*/
-
-        
         BsonClassMap.RegisterClassMap<Pergunta>(cm =>
         {
             cm.AutoMap();
@@ -50,6 +25,7 @@ public static class MongoDbConfigurator
             cm.MapMember(p => p.Codigo).SetIsRequired(true);
             cm.MapMember(p => p.Inicio).SetIsRequired(true);
             cm.MapMember(p => p.Fim).SetIsRequired(true);
+            cm.MapMember(p => p.Ativa);
             cm.MapMember(p => p.Perguntas);
         });
     }
