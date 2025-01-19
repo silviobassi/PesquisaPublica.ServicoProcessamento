34c7488d9e2d629bf0626f2e91ba2a19eacfc466
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2025-01-19 00:15:03 -0300

    feature: criando classe para encapsular erros de validação

---
+++ src/Shared/ServicoProcessamento.Communication/Errors/ValidacaoError.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace ServicoProcessamento.Communication.Errors;
+
+public class ValidacaoError(List<string> errorsMessage)
+    : AppError("Erro de validação", ErrorType.Validation, nameof(ValidacaoError))
+{
+    public override List<string> GetErrorsMessage() => errorsMessage;
+    
+    public override HttpStatusCode GetHttpStatusCode() => HttpStatusCode.BadRequest;
+}
