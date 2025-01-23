faf7a79efb04c6a8e51aaa782ef3c452935038da
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2025-01-22 23:37:24 -0300

    fature: 🧩 criando validação para criação de pesquisas

---
+++ src/Backend/ServicoProcessamento.Application/Pesquisa/CreatePesquisa/CreatePesquisaValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using ServicoProcessamento.Communication.Requests;
+
+namespace ServicoProcessamento.Application.Pesquisa.CreatePesquisa;
+
+public class CreatePesquisaValidator : AbstractValidator<CreatePesquisaRequest>
+{
+    public CreatePesquisaValidator()
+    {
+        RuleFor(pesquisa => pesquisa.Codigo).NotEmpty().WithMessage("Código da pesquisa não pode ser vazio.");
+        RuleFor(pesquisa => pesquisa.Inicio).NotEmpty().WithMessage("Data de início da pesquisa não pode ser vazia.");
+        RuleFor(pesquisa => pesquisa.Fim).NotEmpty().WithMessage("Data de fim da pesquisa não pode ser vazia.");
+    }
+}
