b3e5e2a6f5272ee0fe17129d49d56a65ba3aff18
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2025-01-23 23:55:09 -0300

    refactor: ➕ adicionando formato de data inválido

--- src/Backend/ServicoProcessamento.Application/Pesquisa/CreatePesquisa/CreatePesquisaValidator.cs
+++ src/Backend/ServicoProcessamento.Application/Pesquisa/CreatePesquisa/CreatePesquisaValidator.cs
@@ -8,7 +8,12 @@ public class CreatePesquisaValidator : AbstractValidator<CreatePesquisaRequest>
     public CreatePesquisaValidator()
     {
         RuleFor(pesquisa => pesquisa.Codigo).NotEmpty().WithMessage("Código da pesquisa não pode ser vazio.");
-        RuleFor(pesquisa => pesquisa.Inicio).NotEmpty().WithMessage("Data de início da pesquisa não pode ser vazia.");
-        RuleFor(pesquisa => pesquisa.Fim).NotEmpty().WithMessage("Data de fim da pesquisa não pode ser vazia.");
+        RuleFor(pesquisa => pesquisa.Inicio).NotEmpty()
+            .WithMessage("Data de início da pesquisa não pode ser vazia.")
+            .Matches(@"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$")
+            .WithMessage("Data de início deve estar no formato 'yyyy-MM-ddTHH:mm:ss'.");
+        RuleFor(pesquisa => pesquisa.Fim).NotEmpty().WithMessage("Data de fim da pesquisa não pode ser vazia.")
+            .Matches(@"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$")
+            .WithMessage("Data de início deve estar no formato 'yyyy-MM-ddTHH:mm:ss'.");
     }
 }
