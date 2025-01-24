f514361e0ec7e5159ee4f5d2e5cae619e798ca01
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2025-01-24 00:11:40 -0300

    refactor: 🗑️ removendo comentário

--- src/Backend/ServicoProcessamento.Application/Pesquisa/RemoverPesquisa/RemoverPesquisaUseCase.cs
+++ src/Backend/ServicoProcessamento.Application/Pesquisa/RemoverPesquisa/RemoverPesquisaUseCase.cs
@@ -8,8 +8,6 @@ public class RemoverPesquisaUseCase(IPesquisaRepository pesquisaRepository) : IR
 {
     public async Task<Result> ExecuteAsync(string idPesquisa)
     {
-        // Se a pesquisa já estiver ativa e com respostas, não deve ser possível excluí-la
-
         var pesquisa = await pesquisaRepository.ObterPesquisaPorIdAsync(idPesquisa);
 
         if (pesquisa is null) return new PesquisaNaoEncontradaError();
