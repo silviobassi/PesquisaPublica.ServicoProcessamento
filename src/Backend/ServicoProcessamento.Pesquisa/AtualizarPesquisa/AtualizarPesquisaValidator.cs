6edb89980e19faf57becee5209efb26ffc31d342
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2025-01-24 00:40:43 -0300

    refactor: 📝 movendo Validador de data a uma classe específica

--- src/Backend/ServicoProcessamento.Pesquisa/AtualizarPesquisa/AtualizarPesquisaValidator.cs
+++ src/Backend/ServicoProcessamento.Pesquisa/AtualizarPesquisa/AtualizarPesquisaValidator.cs
@@ -1,7 +1,8 @@
 ﻿using FluentValidation;
 using ServicoProcessamento.Communication.Requests;
+using ServicoProcessamento.Pesquisa.Extensions;
 
-namespace ServicoProcessamento.Application.Pesquisa.AtualizarPesquisa;
+namespace ServicoProcessamento.Pesquisa.AtualizarPesquisa;
 
 public class AtualizarPesquisaValidator : AbstractValidator<AtualizarPesquisaRequest>
 {
@@ -10,10 +11,8 @@ public class AtualizarPesquisaValidator : AbstractValidator<AtualizarPesquisaReq
         RuleFor(pesquisa => pesquisa.Id).NotEmpty().WithMessage("Id da pesquisa não pode ser vazio.");
         RuleFor(pesquisa => pesquisa.Codigo).NotEmpty().WithMessage("Código da pesquisa não pode ser vazio.");
 
-        RuleFor(pesquisa => pesquisa.Inicio)
-            .NotEmpty().WithMessage("Data do início da pesquisa não pode ser vazia.")
-            .Matches(@"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$")
-            .WithMessage("Data do início deve estar no formato 'yyyy-MM-ddTHH:mm:ss'.");
+        RuleFor(pesquisa => pesquisa.Inicio)!.ValidatorDataInicio();
+        RuleFor(pesquisa => pesquisa.Fim)!.ValidatorDataFim();
 
         RuleFor(pesquisa => pesquisa.Fim)
             .NotEmpty().WithMessage("Data do fim da pesquisa não pode ser vazia.")
---
+++ src/Backend/ServicoProcessamento.Pesquisa/CreatePesquisa/CreatePesquisaValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using ServicoProcessamento.Communication.Requests;
+using ServicoProcessamento.Pesquisa.Extensions;
+
+namespace ServicoProcessamento.Pesquisa.CreatePesquisa;
+
+public class CreatePesquisaValidator : AbstractValidator<CreatePesquisaRequest>
+{
+    public CreatePesquisaValidator()
+    {
+        RuleFor(pesquisa => pesquisa.Codigo).NotEmpty().WithMessage("Código da pesquisa não pode ser vazio.");
+
+        RuleFor(pesquisa => pesquisa.Inicio)!.ValidatorDataInicio();
+        RuleFor(pesquisa => pesquisa.Fim)!.ValidatorDataFim();
+    }
+}
