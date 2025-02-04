fc9673174d6b1d671aa3924a579e32d41474f004
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2025-02-04 10:59:27 -0300

    refactor: adicionando Railway Pattern

--- src/Backend/ServicoProcessamento.Feature/Pesquisa/AtualizarPesquisa/AtualizarPesquisaUseCase.cs
+++ src/Backend/ServicoProcessamento.Feature/Pesquisa/AtualizarPesquisa/AtualizarPesquisaUseCase.cs
@@ -1,8 +1,10 @@
-﻿using E7.EasyResult;
-using E7.EasyResult.Errors;
+﻿using ServicoProcessamento.Communication.E7.EasyResult;
+using ServicoProcessamento.Communication.E7.EasyResult.Errors;
 using ServicoProcessamento.Communication.Pesquisa.Errors;
 using ServicoProcessamento.Communication.Pesquisa.Requests;
-using ServicoProcessamento.Domain.Pesquisa;
+using ServicoProcessamento.Domain.Pesquisa.Entities;
+using ServicoProcessamento.Domain.Pesquisa.Factories;
+using ServicoProcessamento.Domain.Pesquisa.Repositories;
 using ServicoProcessamento.Feature.Pesquisa.Extensions;
 
 namespace ServicoProcessamento.Feature.Pesquisa.AtualizarPesquisa;
@@ -11,29 +13,35 @@ public class AtualizarPesquisaUseCase(IPesquisaRepository pesquisaRepository) : 
 {
     public async Task<Result> ExecuteAsync(AtualizarPesquisaRequest request)
     {
-        var result = await ValidateAsync(request);
+        var validateAsync = await ValidateAsync(request);
 
-        if (result.IsFailure) return Result.Failure(result.Error);
-
-        var pesquisa = new Domain.Pesquisa.Pesquisa(request.Codigo, request.InicioAsDateTimeOffset,
-            request.FimAsDateTimeOffset);
-
-        pesquisa.ObterId(request.Id);
-
-        pesquisa = await pesquisaRepository.AtualizarPesquisaAsync(pesquisa);
-
-        return pesquisa is null
-            ? new PesquisaNaoEncontradaError()
-            : Result.Success();
+        return validateAsync.MapFailure(x => x)
+            .Map(_ => pesquisaRepository.ObterPesquisaPorIdAsync(request.Id))
+            .Bind(VerificarSePesquisaExiste)
+            .Bind(VerificarSePesquisaEstaSendoRespondida)
+            .Map(_ => PesquisaFactory.Atualizar(request.Id, request.Codigo, request.InicioAsDateTimeOffset,
+                request.FimAsDateTimeOffset))
+            .Map(pesquisaRepository.AtualizarPesquisaAsync)
+            .Bind(VerificarSePesquisaFoiAtualizada);
     }
-
-    private static async Task<Result> ValidateAsync(AtualizarPesquisaRequest request)
+    
+    private static async Task<Result<bool>> ValidateAsync(AtualizarPesquisaRequest request)
     {
         var validator = new AtualizarPesquisaValidator();
         var response = await validator.ValidateAsync(request);
 
         return response.IsValid.IsFalse()
             ? new InvalidFieldsError(response.Errors.Select(error => error.ErrorMessage).Distinct().ToList()!)
-            : Result.Success();
+            : Result<bool>.Success(true);
     }
+
+    private static Result<PesquisaEntity?> VerificarSePesquisaExiste(PesquisaEntity? pesquisa)
+        => pesquisa is null ? new PesquisaNaoEncontradaError() : pesquisa;
+
+    private static Result<PesquisaEntity?> VerificarSePesquisaEstaSendoRespondida(
+        PesquisaEntity? pesquisa)
+        => pesquisa!.Ativa && pesquisa.EstaSendoRespondida ? new PesquisaEstaSendoRespondidaError() : pesquisa;
+
+    private static Result<bool> VerificarSePesquisaFoiAtualizada(long modifiedCount)
+        => modifiedCount == 0 ? new PesquisaNaoFoiAtualizada() : Result<bool>.Success(true);
 }
