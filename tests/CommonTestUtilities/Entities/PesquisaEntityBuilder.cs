f006354ee371e3b98c08489e67b3dbb4e60249ba
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2025-02-04 18:16:48 -0300

    refactor: removendo ObjectId do mongodb

--- tests/CommonTestUtilities/Entities/PesquisaEntityBuilder.cs
+++ tests/CommonTestUtilities/Entities/PesquisaEntityBuilder.cs
@@ -3,12 +3,12 @@ using ServicoProcessamento.Domain.Pesquisa.Entities;
 
 namespace CommonTestUtilities.Entities;
 
-public class PesquisaEntityBuilder
+public abstract class PesquisaEntityBuilder
 {
     public static PesquisaEntity Build(DateTimeOffset? inicio = null, DateTimeOffset? fim = null)
     {
         var pesquisa = new Faker<PesquisaEntity>()
-            .RuleFor(pesquisa => pesquisa.Id, f => f.Random.Word())
+            .RuleFor(pesquisa => pesquisa.Id, _ => Guid.NewGuid().ToString())
             .RuleFor(pesquisa => pesquisa.Codigo, f => f.Random.Word())
             .RuleFor(pesquisa => pesquisa.Inicio, _ => inicio)
             .RuleFor(pesquisa => pesquisa.Fim, _ => fim)
