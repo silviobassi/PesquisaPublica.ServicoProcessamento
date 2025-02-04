97583ce78ef933f3946a2c5bc4b970476da284f7
Silvio Luiz Bassi <silviobassi1@hotmail.com>
2025-02-04 18:13:10 -0300

    refactor: alterando AtualizarPesquisaRequest

--- src/Shared/ServicoProcessamento.Communication/Pesquisa/Requests/AtualizarPesquisaRequest.cs
+++ src/Shared/ServicoProcessamento.Communication/Pesquisa/Requests/AtualizarPesquisaRequest.cs
@@ -2,8 +2,13 @@ using System.Globalization;
 
 namespace ServicoProcessamento.Communication.Pesquisa.Requests;
 
-public record AtualizarPesquisaRequest(string Id, string Codigo, string? Inicio, string? Fim)
+public record AtualizarPesquisaRequest
 {
+    public string Id { get; } = string.Empty;
+    public string Codigo { get; } = string.Empty;
+    public string? Inicio { get; } = string.Empty;
+    public string? Fim { get; } = string.Empty;
+
     public DateTimeOffset? InicioAsDateTimeOffset =>
         DateTimeOffset.TryParse(Inicio, CultureInfo.InvariantCulture, DateTimeStyles.None, out var result)
             ? result
